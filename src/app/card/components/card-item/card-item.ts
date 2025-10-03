81147e147a4d33d883429cff7435d793978a5dd9
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2025-10-02 19:04:54 -0500

    first commit

---
+++ src/app/card/components/card-item/card-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-item',
+  imports: [],
+  templateUrl: './card-item.html',
+  styleUrl: './card-item.css'
+})
+export class CardItem {
+
+}
